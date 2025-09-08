2b06dc24284a1a791132b1d89e765a65e6a4b069
farial-robama <farialrobama15@gmail.com>
2025-09-08 15:40:37 +0200

    Load and display plants by category

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,25 @@ const loadTreeCategories = () => {
     .catch((err) => console.error("Error loading categories:", err));
 };
 
-// const loadAllPlants =() => {
-//     fetch(`https://openapi.programming-hero.com/api/plants`)
-//     .then((res) => res.json())
-//     .then((json) => {
-//         const plantsArray = Array.isArray(json.plants) ? json.plants : [json.plants];
-//         displayPlants(plantsArray)
-//     })
-//     .catch((err) => console.error("Error loading category:", err));
-// }
+const loadAllPlants =() => {
+    fetch(`https://openapi.programming-hero.com/api/plants`)
+    .then((res) => res.json())
+    .then((json) => {
+        const plantsArray = Array.isArray(json.plants) ? json.plants : [json.plants];
+        displayPlants(plantsArray)
+    })
+    .catch((err) => console.error("Error loading category:", err));
+}
+
+const loadPlantsByCategory = (categoryId) => {
+    fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
+    .then((res) => res.json())
+    .then((json) => {
+        const plantsArray = Array.isArray(json.plants) ? json.plants : [json.plants];
+        displayPlants(plantsArray)
+    })
+    .catch((err) => console.error("Error loading category:", err));
+}
 
 const displayCategories = (categories) => {
     const treeCategories = document.getElementById("treeCategories");
@@ -59,50 +69,39 @@ const displayCategories = (categories) => {
 };
 
 
+const displayPlants = (plants) => {
+    const plantsContainer = document.getElementById("plantsContainer");
+    plantsContainer.innerHTML = "";
 
-// const loadPlantsByCategory = (categoryId) => {
-//     fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
-//     .then((res) => res.json())
-//     .then((json) => {
-//         const plantsArray = Array.isArray(json.plants) ? json.plants : [json.plants];
-//         displayPlants(plantsArray)
-//     })
-//     .catch((err) => console.error("Error loading category:", err));
-// }
-
-// const displayPlants = (plants) => {
-//     const plantsContainer = document.getElementById("plantsContainer");
-//     plantsContainer.innerHTML = "";
-
-//     for (const plant of plants) {
-//         const card = document.createElement("div");
-//         card.innerHTML = `
-//         <div class="card bg-base-100 shadow-sm h-[400px]">
-//               <figure class="px-4 pt-4 overflow-hidden ">
-//                 <img class="rounded-md object-cover h-full w-full"
-//                   src="${plant.image}"
-//                   alt="${plant.name}"
-//                 />
-//               </figure>
-//               <div class="card-body flex flex-col justify-between flex-1">
-//                 <h2 class="card- font-bold text-base">${plant.name}</h2>
-//                 <p class="text-xs font-light line-clamp-5">
-//                   ${plant.description}
-//                 </p>
-//                 <div class="card-actions justify-between">
-//                   <div class="badge badge-outline bg-[#DCFCE7] text-[#15803D] border-none">
-//                     ${plant.category}
-//                   </div>
-//                   <div class="price">৳${plant.price}</div>
-//                 </div>
-//                 <button class="btn btn-primary bg-[#15803D] text-[#FFFFFF] border-none">
-//                   Add to Cart
-//                 </button>
-//               </div>
-//             </div>
-//         `;
-//         plantsContainer.appendChild(card);
-//     };
-// };
+    for (const plant of plants) {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="card bg-base-100 shadow-sm h-[400px]">
+              <figure class="px-4 pt-4 overflow-hidden ">
+                <img class="rounded-md object-cover h-full w-full"
+                  src="${plant.image}"
+                  alt="${plant.name}"
+                />
+              </figure>
+              <div class="card-body flex flex-col justify-between flex-1">
+                <h2 class="card- font-bold text-base">${plant.name}</h2>
+                <p class="text-xs font-light line-clamp-5">
+                  ${plant.description}
+                </p>
+                <div class="card-actions justify-between">
+                  <div class="badge badge-outline bg-[#DCFCE7] text-[#15803D] border-none">
+                    ${plant.category}
+                  </div>
+                  <div class="price">৳${plant.price}</div>
+                </div>
+                <button class="btn btn-primary bg-[#15803D] text-[#FFFFFF] border-none">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+        `;
+        plantsContainer.appendChild(card);
+    };
+};
 
 loadTreeCategories()
